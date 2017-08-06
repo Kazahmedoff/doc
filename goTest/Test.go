b12de61cc45507c4237d35359a4885f0adef273b
Kirill Kazakhmedov <k.kazahmedoff@mail.ru>
2017-08-06 16:52:49 +0300

    test

---
+++ goTest/Test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	path := scanner.Text()
+	fmt.Println(getStringCount(path))
+}
+
+func getStringCount(path string) int {
+	content, error := ioutil.ReadFile(path)
+
+	if error != nil {
+		fmt.Println("This file is not exist!")
+		os.Exit(1)
+	}
+
+	text := string(content)
+	count := strings.Count(text, "\n") + 1
+	return  count
+}
